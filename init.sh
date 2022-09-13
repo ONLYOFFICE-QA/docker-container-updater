91cfc6beb207e8a6e6891e9abc7bc04ce3244b8b
Pavel Lobashov <shockwavenn@gmail.com>
2022-09-13 15:09:32 +0300

    Prune all old volumes via single command

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 docker stop 4testing-documentserver-ee docker-container-updater
 docker rm 4testing-documentserver-ee docker-container-updater
 docker system prune -af
-docker volume rm "$(docker volume ls -qf dangling=true)"
+docker volume prune -f
 docker build -t docker-container-updater .
 docker run --privileged --shm-size=2g --restart=always -itd \
        -v /var/run/docker.sock:/var/run/docker.sock \
